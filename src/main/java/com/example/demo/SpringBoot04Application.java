83655d1229b67b09cb6e3502791cc8a1c401d20b
dwayne1516 <dariuswscott@gmail.com>
2017-12-08 04:47:21 -0800

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot04Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot04Application.class, args);
+	}
+}
---
+++ src/test/java/com/example/demo/HomeControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import static org.junit.Assert.*;
+
+public class HomeControllerTest {
+
+}
